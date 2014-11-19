bcf61d5e14ed97947ba9d0d2b5b698b77f7add25
arataitou <aito.gc4@gmail.com>
2014-11-19 01:59:47 +0000

    event参加取り消し機能追加(Auth関係以外)

--- app/Controller/EventsController.php
+++ app/Controller/EventsController.php
@@ -217,16 +217,29 @@ class EventsController extends AppController {
     }
 
     public function cancel($id = null){
-        if ($this->Event->exists($id)) {
+        $this->Event->id = $id;
+        if (!$this->Event->exists($id)) {
             throw new NotFoundException(__('Invlid event'));
         }
-        if ($this->request->is(array('post', 'put'))) {
-            if ($this->Event->save($this->request->data)) {
-                $this->Session->setFlash(__('You canceled to participate this event.'));
-                return $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash(__('You could not cancel to participate this event.'));
-            }
+
+        $userJoinEvent = $this->Participants->find('first', array(
+            'conditions' => array('event_id' => $id,
+            //'user_id' => $this->Auth->user('id'),
+            //'status' => 'true'
+        ),//user_id、statusの検索条件追加
+            )
+        );
+
+        $cancelInfo = array(
+            "id" => $userJoinEvent['Particpant']['id'], 
+            "status" => 'false'
+        );
+
+        if ($this->Participants->save($cancelInfo)) {
+            $this->Session->setFlash(__('You canceled to participate this event.'));
+            return $this->redirect(array('action' => 'index'));
+        } else {
+            $this->Session->setFlash(__('You could not cancel to participate this event.'));
         }
     }
 }

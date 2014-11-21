0f735382cbe2aeb705e36647607dbefdc8d77ef2
SoneKosuke <ksone.gc4@gmail.com>
2014-11-21 01:54:20 +0000

    review 修正

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,28 +1,16 @@
 <?php
 App::uses('AppModel', 'Model');
-App::uses('SimplePasswordHasher','Controller/Component/Auth');
-
+App::uses('SimplePasswordHasher', 'Controller/Component/Auth');
 /**
  * User Model
  *
  */
 class User extends AppModel {
-
 /**
  * Display field
  *
  * @var string
  */
-
-    // ユーザ登録時にパスワードをハッシュ化
-    public function beforeSave($options = array()){
-        if(isset($this->data[$this->alias]['password'])){
-            $passwordHasher = new SimplePasswordHasher();
-            $this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']);
-        }
-        return true;
-    }    
-
     public $validate = array(
         'name' => array(
             'required' => array(
@@ -37,9 +25,16 @@ class User extends AppModel {
             )
         )
     );
-   
     public function isOwnedBy($user, $user) {
-    return $this->field('id', array('id' => $user, 'id' => $user)) !== false;
+        return $this->field('id', array('id' => $user, 'id' => $user)) !== false;
     }
+    // ユーザ登録時にパスワードをハッシュ化
+    public function beforeSave($options = array()){
+        if (isset($this->data[$this->alias]['password'])) {
+            $passwordHasher = new SimplePasswordHasher();
+            $this->data[$this->alias]['password'] = $passwordHasher->hash($this->data[$this->alias]['password']);
+        }
+        return true;
+    }    
 }
 
